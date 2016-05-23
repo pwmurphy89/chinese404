4525abde1923038a21dc8bc522aa455a840a80f5
pwmurphy89 <pwmurphy89@gmail.com>
2016-05-23 13:23:23 -0400

    url change

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
   <nav class="navbar navbar-default">
     <div id="header" class="container-fluid">
       <div class="navbar-header col-sm-7">
-        <a class="navbar-brand" href="/index.php">Chinese404</a>
+        <a class="navbar-brand" href="chinese404/index.php">Chinese404</a>
       </div>
       <ul class="nav navbar-nav col-sm-5 list-inline">
         <?php
