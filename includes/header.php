3dabaacaa638007a144e27214ba067d76c070d18
pwmurphy89 <pwmurphy89@gmail.com>
2016-05-23 13:45:01 -0400

    url test

--- includes/header.php
+++ includes/header.php
@@ -3,13 +3,13 @@
   <nav class="navbar navbar-default">
     <div id="header" class="container-fluid">
       <div class="navbar-header col-sm-7">
-        <a class="navbar-brand" href="index.php">Chinese404</a>
+        <a class="navbar-brand" href="/index.php">Chinese404</a>
       </div>
       <ul class="nav navbar-nav col-sm-5 list-inline">
         <?php
           if(isset($_SESSION['username'])){
             print'<li class="nav-options welcome">Welcome ' .$_SESSION['username']. '!'.'</li>';
-            print'<li class="nav-options"><a href="/">Posts</a></li>';
+            print'<li class="nav-options"><a href="index.php">Posts</a></li>';
             print '<li class="nav-options"><a href="follow.php">Follow</a></li>'; 
             print '<li class="nav-options"><a href="logout.php">Log Out</a></li>';
 
